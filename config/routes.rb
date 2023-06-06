1b61d10eae50833e3789fc1c39a794189107beec
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-06-06 11:12:21 +0100

    Routes finished with added destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
   devise_for :users
   root "crawls#home"
 
   resources :users, only: [:dashboard] do
     resources :favourites, only: [:new, :create]
+    resources :reviews, only: [:destroy]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   resources :crawls, only: [:home, :index, :new, :create ] do
     resources :reviews, only: [:new, :create]
   end
