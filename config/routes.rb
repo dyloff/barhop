366dbff12f229dfa54885240354a3dee14787447
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-06-06 11:06:15 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  get 'favourites/new'
-  get 'favourites/create'
-  get 'users/dashboard'
-  get 'shared_withs/share'
-  get 'shared_withs/confirm'
-  get 'crawls/home'
-  get 'crawls/index'
-  get 'crawls/new'
-  get 'crawls/create'
   devise_for :users
-  root to: "pages#home"
+  root "crawls#home"
+
+  resources :users, only: [:dashboard] do
+    resources :favourites, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :crawls, only: [:home, :index, :new, :create ] do
+    resources :reviews, only: [:new, :create]
+  end
 end
