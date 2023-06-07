a60c2d2dc5c6d4b0b6fa5bf51f4e5ae24a256258
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-06-07 18:18:05 +0100

    Filters backend started

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -17,7 +17,42 @@ class CrawlsController < ApplicationController
   end
 
   def new
-    raise
+
+    # @bars_address_search = Bar.search_by_address(params[:query]) if params[:query].present?
+
+    bars_by_price = Bar.all.map do |bar|
+      params[:price].contains?(bar.price_range)
+    end
+
+    @bars_price_range_search = Bar.filter_by_price(params[:price_range]) if params[:price_range].present?
+    @bars_venue_search = Bar.filter_by_venue(params[:venue_category]) if params[:venue_category].present?
+    @bars_number = params[:number_of_bars].to_i
+    @bars_day = params[:day]
+
+    @bars_address_price_venue = @bars_price_range_search & @bars_venue_search
+
+    # if params[:price_range].present? && params[:venue_category].present? && params[:number_of_bars].present? && params[:day].present? && params[:query].present?
+    #     @crawls_address_search = Crawl.search_by_address(params[:query])
+    #     @crawls_price_range = Crawl.filter_by_price(params[:price_range])
+    #     @crawls_venue_category = Crawl.includes(:user).joins(:user).where(users: { first_name: params[:artist] })
+    #     @crawls = @crawls_p & @crawls_a
+
+    #   elsif params[:price].present?
+    #     @crawls = crawl.filter_by_price(params[:price]) if params[:price].present?
+    #   elsif params[:artist].present?
+    #     @crawls = crawl.includes(:user).joins(:user).where(users: { first_name: params[:artist] })
+    #   else
+    #     @crawls = crawl.all
+    #   end
+
+    #   if params[:query].present?
+    #     @crawls = crawl.search_by_address(params[:query])
+    #   else
+    #     @crawls_false_search = crawl.all
+    #   end
+    #   @crawls_false_search = crawl.all
+    #   end
+
     @bars = Bar.all
   end
 
