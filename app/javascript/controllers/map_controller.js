d0a868e867ac48abe5918e3d34f31935cd51965e
Dylon M <dylonmura@gmail.com>
2023-06-07 16:33:44 +0100

    dylon-mapbox-finished

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 import mapboxgl from 'mapbox-gl'
 
 
+// fetch ("https://api.mapbox.com/directions/v5/")
+//   .then (response => response.json ())
+//   .then (data = {
+//   });
+
 
 export default class extends Controller {
   static values = {
@@ -18,64 +23,148 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
     });
+
       this.#addMarkersToMap()
       this.#fitMapToMarkers()
-      this.#addLayer()
+      // this.#addLayer()
+      // this.#getRoute()
       // this.#addDirections()
       // this.#addControl()
-  }
 
-  #addMarkersToMap(){
-    console.log(this.markersValue)
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      new mapboxgl.Marker()
+      let fetchQueryString = "https://api.mapbox.com/directions/v5/mapbox/walking/";
+
+      let i = 1;
+      this.markersValue.forEach((marker) => {
+        if (i == this.markersValue.length) {
+          fetchQueryString = fetchQueryString + marker.lng + "," + marker.lat + `?geometries=geojson&access_token=${this.apiKeyValue}`;
+        } else {
+          fetchQueryString = fetchQueryString + marker.lng + "," + marker.lat + ";";
+        }
+        i += 1
+      });
+
+      console.log(fetchQueryString)
+      this.#getRoute(fetchQueryString)
+    }
+
+    #addMarkersToMap(){
+      console.log(this.markersValue)
+      this.markersValue.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+        new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.map)
-  })
-  }
+      })
+    }
 
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
+    #fitMapToMarkers() {
+      const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 
-  #addLayer() {
+  #getRoute(fetchQueryString) {
+    fetch(fetchQueryString)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
 
-    const routesCoodinates = [];
+        const routes = data.routes[0].geometry.coordinates;
+        console.log("This is routes");
+        console.log(routes);
 
-    this.markersValue.forEach((marker) => {
-      routesCoodinates.push([marker.lng, marker.lat])
-    })
+        this.map.on('load', () => {
+          this.map.addSource('route', {
+          'type': 'geojson',
+          'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+          'type': 'LineString',
+          'coordinates': routes
+          }
+          }
+        });
 
-    this.map.on('load', () => {
-      this.map.addSource('route', {
-      'type': 'geojson',
-      'data': {
-      'type': 'Feature',
-      'properties': {},
-      'geometry': {
-      'type': 'LineString',
-      'coordinates': routesCoodinates
-      }
-      }
-    });
+          this.map.addLayer({
+          'id': 'route',
+          'type': 'line',
+          'source': 'route',
+          'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+          },
+          'paint': {
+          'line-color': '#888',
+          'line-width': 8
+          }
+          });
+        });
 
-      this.map.addLayer({
-      'id': 'route',
-      'type': 'line',
-      'source': 'route',
-      'layout': {
-      'line-join': 'round',
-      'line-cap': 'round'
-      },
-      'paint': {
-      'line-color': '#888',
-      'line-width': 8
-      }
-      });
-    });
-  }
+
+        // const geojson = {
+        //   type: "Feature",
+        //   properties: {},
+        //   geometry: {
+        //     type: "LineString",
+        //     coordinates: routes,
+        //   }
+        // }
+
+        // this.map.addLayer({
+        //   id: "routes",
+        //   type: "line",
+        //   source: {
+        //     type: "geojson",
+        //     data: geojson
+        //   },
+        //   layout: {
+        //     "line-join": "round",
+        //     "line-cap": "round",
+        //   },
+        //   paint: {
+        //     "line-color": "#D94124",
+        //     "line-width": 5,
+        //     "line-opacity": 0.75,
+        //   }
+        // })
+      })
+
+      // const routesCoodinates = [];
+      // this.markersValue.forEach((marker) => {
+      //   routesCoodinates.push([marker.lng, marker.lat])
+      // })
+
+      // console.log("This is routesCoodinates")
+      // console.log(routesCoodinates)
+
+      // this.map.on('load', () => {
+      //   this.map.addSource('route', {
+      //   'type': 'geojson',
+      //   'data': {
+      //   'type': 'Feature',
+      //   'properties': {},
+      //   'geometry': {
+      //   'type': 'LineString',
+      //   'coordinates': routesCoodinates
+      //   }
+      //   }
+      // });
+
+      //   this.map.addLayer({
+      //   'id': 'route',
+      //   'type': 'line',
+      //   'source': 'route',
+      //   'layout': {
+      //   'line-join': 'round',
+      //   'line-cap': 'round'
+      //   },
+      //   'paint': {
+      //   'line-color': '#888',
+      //   'line-width': 8
+      //   }
+      //   });
+      // });
+    }
 }
