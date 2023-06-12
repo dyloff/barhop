798ef9a748308a0bf43f68bd7564bea61ec793ae
Dylon M <dylonmura@gmail.com>
2023-06-12 16:59:18 +0100

    test

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -116,9 +116,9 @@ class CrawlsController < ApplicationController
   private
 
   def filters
+    @master_bar_list = retrieve_bars_from_api
     if params[:venue_category].include?("restaurant")
       # @all_bars_test = retrieve_bars_from_api
-      @master_bar_list = retrieve_bars_from_api
       @bars_by_venue = @master_bar_list
     # elsif params[:venue_category].include?("restaurant")
     #   @bars_by_venue = Bar.all.select { |bar| bar.types.include?('restaurant') }
@@ -144,6 +144,7 @@ class CrawlsController < ApplicationController
     # Number of bars requested
     @number_of_bars = params[:number_of_bars] == "" ? 3 : params[:number_of_bars].to_i
     @filtered_bars = @all_filtered_bars.sample(@number_of_bars)
+
   end
 
   def google_api_call( pars = {} )
