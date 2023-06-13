95f0a782c9ef722a198ef696c93008fbb28a0e6e
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-06-13 15:54:46 +0100

    Locking working

--- app/javascript/controllers/reload_controller.js
+++ app/javascript/controllers/reload_controller.js
@@ -1,30 +1,88 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+// import Rails from "rails-ujs";
 
 // Connects to data-controller="reload"
 export default class extends Controller {
-  static targets = ["barsList"]
+  static targets = ["barsList"];
   static values = {
-    bars: Array
-  }
-  url = `${window.location}&bars=${this.barsValue}`
+    bars: Array,
+  };
+
+  url = `${window.location}&bars=${this.barsValue}`;
 
   connect() {
-    console.log("HIIII")
+    console.log("This is from reload stimulus");
+    // console.log(this.barsListTarget);
   }
 
+  keepBar(event) {
+    // console.log(this.barsValue);
+    // console.log(this.url);
 
-  test(event) {
-    console.log(this.url)
-    this.url = this.url + `&bar_${event.currentTarget.dataset.bar}=saved`
-    console.log(this.url)
+    console.log(event.currentTarget.dataset.bar);
+
+    // * This dataset.bar is from "_barcards_regeneration.html.erb"
+    console.log(event.currentTarget.dataset.barIndex);
+
+    this.url = this.url + `&bar_${event.currentTarget.dataset.bar}=saved`;
+    console.log(this.url);
+    event.currentTarget.classList.add("selectedBarColor")
   }
 
   generate() {
-    fetch(this.url, { headers: { 'Accept': 'text/plain' } })
-    .then(response => response.text())
-    .then((data) => {
-      this.barsListTarget.outerHTML = data;
-    })
+    fetch(this.url, { headers: { Accept: "text/plain" } })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        this.element.outerHTML = data;
+
+        // this.barsListTarget.outerHTML = data;
+
+        const mapContainer = document.querySelector(".crawl-map");
+        console.log(mapContainer);
+
+        console.log(this.element);
+
+        document.querySelectorAll(".new-crawl-bars-map").forEach((map) => {
+          map.classList.add("d-none");
+        });
+
+        const barsMaps = document.querySelectorAll(".new-crawl-bars-map");
+
+        console.log(barsMaps[barsMaps.length - 1]);
+
+        barsMaps[barsMaps.length - 1].classList.remove("d-none");
+
+        // Rails.ajax({
+        //   url: this.data.get("url").replace(":id", id),
+        //   type: "PATCH",
+        //   data: data,
+        //   success: function () {
+        //     console.log("Try to get a updated map");
+        //     // document.location.reload();
+
+        //     fetch(`/routes/${routeId}/map`)
+        //       // .then((res) => res.json())
+        //       // .then((data) => {
+        //       //   console.log(data.json);
+        //       //   console.log(data.template);
+        //       //   console.log(data.map);
+        //       // });
+        //       .then((res) => {
+        //         console.log(res);
+        //         // console.log(res.text());
+        //         return res.text();
+        //       })
+        //       .then((data) => {
+        //         console.log("Checking Fetch Get Response");
+        //         // console.log(data);
+
+        //         mapContainer.innerHTML = data;
+        //         // mapContainer.insertAdjacentHTML("beforeend", data);
+        //       });
+        //   },
+        // });
+      });
   }
 
   reload() {
