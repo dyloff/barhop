36d868406318ee7a0e99832dd81b3e2e24aff7bd
Alek Pialucha <pialuchaa@btinternet.com>
2023-06-15 15:55:50 +0100

    fixed the regen

--- app/javascript/controllers/generate_button_controller.js
+++ app/javascript/controllers/generate_button_controller.js
@@ -2,15 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="generate-button"
 export default class extends Controller {
-  static targets = ["button", "spinner"]
+  static targets = ["button", "spinner", "page"]
+
   connect() {
     // console.log("this is long")
     console.log(this.buttonTarget)
+    // console.log(this.spinnerTarget)
+    // console.log(this.pageTarget)
+
+    const spinner = document.querySelector("#spinner")
+    const page = document.querySelector("#page")
+
+    console.log(page)
+
+    spinner.className = "container loading-page hidden"
+    page.className = ""
+
   }
 
   generate() {
-    this.spinnerTarget.classList.remove("invisible")
-    this.buttonTarget.classList.add(disabled)
+    console.log("generate function")
+
+    const spinner = document.querySelector("#spinner")
+    const page = document.querySelector("#page")
+
+    spinner.classList.toggle("hidden");
+    page.classList.toggle("hidden");
+    
   }
 
 
