4a7bdb028a0ce2c094b062386bcd6386745e78ec
Dylon M <dylonmura@gmail.com>
2023-06-15 14:57:58 +0100

    custom map color

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
-import mapboxgl from 'mapbox-gl'
+import { Controller } from "@hotwired/stimulus";
+import mapboxgl from "mapbox-gl";
 
 // fetch ("https://api.mapbox.com/directions/v5/")
 //   .then (response => response.json ())
@@ -9,90 +9,109 @@ import mapboxgl from 'mapbox-gl'
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
-  }
+    markers: Array,
+  };
 
   connect() {
     // console.log("This is from map")
 
-    mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue;
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
     });
 
-      this.#addMarkersToMap()
-      this.#fitMapToMarkers()
-      // this.#addLayer()
-      // this.#getRoute()
-      // this.#addDirections()
-      // this.#addControl()
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+    // this.#addLayer()
+    // this.#getRoute()
+    // this.#addDirections()
+    // this.#addControl()
 
-      let fetchQueryString = "https://api.mapbox.com/directions/v5/mapbox/walking/";
+    let fetchQueryString =
+      "https://api.mapbox.com/directions/v5/mapbox/walking/";
 
-      let i = 1;
-      this.markersValue.forEach((marker) => {
-        if (i == this.markersValue.length) {
-          fetchQueryString = fetchQueryString + marker.lng + "," + marker.lat + `?geometries=geojson&access_token=${this.apiKeyValue}`;
-        } else {
-          fetchQueryString = fetchQueryString + marker.lng + "," + marker.lat + ";";
-        }
-        i += 1
+    let i = 1;
+    this.markersValue.forEach((marker) => {
+      if (i == this.markersValue.length) {
+        fetchQueryString =
+          fetchQueryString +
+          marker.lng +
+          "," +
+          marker.lat +
+          `?geometries=geojson&access_token=${this.apiKeyValue}`;
+      } else {
+        fetchQueryString =
+          fetchQueryString + marker.lng + "," + marker.lat + ";";
+      }
+      i += 1;
+    });
+
+    // console.log(fetchQueryString)
+    this.#getRoute(fetchQueryString);
+
+    // favBtn.addEventListener("click", () => {
+    //   this.map.resize();
+    // })
+    // toggleBtn.addEventListener("click", () => {
+    //   this.map.resize();
+    // })
+
+    const indexBtns = document.querySelectorAll(".index-btn");
+    console.log("Testing");
+    console.log(indexBtns);
+
+    indexBtns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        console.log("Index btn clicked");
+        this.map.resize();
       });
+    });
+  }
 
-      // console.log(fetchQueryString)
-      this.#getRoute(fetchQueryString)
+  #addMarkersToMap() {
+    // console.log(this.markersValue)
 
-      // favBtn.addEventListener("click", () => {
-      //   this.map.resize();
-      // })
-      // toggleBtn.addEventListener("click", () => {
-      //   this.map.resize();
-      // })
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
 
-      const indexBtns = document.querySelectorAll(".index-btn")
-      console.log("Testing");
-      console.log(indexBtns);
+      const customMarker = document.createElement("div");
+      // customMarker.className = 'marker';
 
-      indexBtns.forEach((btn) => {
-        btn.addEventListener("click", () => {
-          console.log("Index btn clicked");
-          this.map.resize();
-      })
-    })
-    }
-
-    #addMarkersToMap(){
-      // console.log(this.markersValue)
-
-      this.markersValue.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-
-        const customMarker = document.createElement("div");
-        customMarker.innerHTML = marker.marker_html;
+      customMarker.innerHTML = marker.marker_html;
 
       //  console.log(customMarker.innerHTML);
 
-        new mapboxgl.Marker(customMarker)
-        new mapboxgl.Marker()
+      new mapboxgl.Marker(customMarker);
+      new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
-        .addTo(this.map)
-      })
-    }
+        .addTo(this.map);
 
-    #fitMapToMarkers() {
-      const bounds = new mapboxgl.LngLatBounds()
-      this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+
+      const markersIcons = document.querySelectorAll(".mapboxgl-marker");
+
+      markersIcons.forEach((icon) => {
+
+
+        icon.firstElementChild.children[2].setAttribute("fill", "#000");
+      });
+    });
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 
   #getRoute(fetchQueryString) {
     fetch(fetchQueryString)
       .then((response) => response.json())
       .then((data) => {
-
         // console.log("This is route duration")
         // console.log(data.routes[0].duration)
 
@@ -109,46 +128,50 @@ export default class extends Controller {
 
         // insertAdjacentHTML("afterend", `<p>Est duration: ${data.routes[0].duration / 60}</p>`);
 
-        this.map.on('load', () => {
-          this.map.addSource('route', {
-          'type': 'geojson',
-          'data': {
-          'type': 'Feature',
-          'properties': {},
-          'geometry': {
-          'type': 'LineString',
-          'coordinates': routes
-          }
-          }
-        });
+        this.map.on("load", () => {
+          this.map.addSource("route", {
+            type: "geojson",
+            data: {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates: routes,
+              },
+            },
+          });
 
           this.map.addLayer({
-          'id': 'route',
-          'type': 'line',
-          'source': 'route',
-          'layout': {
-          'line-join': 'round',
-          'line-cap': 'round'
-          },
-          'paint': {
-          'line-color': '#D94124',
-          'line-width': 5,
-          }
+            id: "route",
+            type: "line",
+            source: "route",
+            layout: {
+              "line-join": "round",
+              "line-cap": "round",
+            },
+            paint: {
+              "line-color": "#D94124",
+              "line-width": 5,
+            },
           });
         });
 
-        let counter = 0
+        let counter = 0;
         this.markersValue.forEach((marker) => {
-          counter += 1
-        })
+          counter += 1;
+        });
 
-        let totalTimeMins = (Math.round(data.routes[0].duration / 60) + (45 * counter)) % 60
-        let totalTimeHours = Math.round((Math.round(data.routes[0].duration / 60) + (45 * counter)) / 60)
+        let totalTimeMins =
+          (Math.round(data.routes[0].duration / 60) + 45 * counter) % 60;
+        let totalTimeHours = Math.round(
+          (Math.round(data.routes[0].duration / 60) + 45 * counter) / 60
+        );
         // console.log("------------------")
         // console.log(totalTimeHours + ":" + totalTimeMins)
         // console.log("------------------")
-        this.element.parentElement.parentElement.parentElement.children[0].querySelector(".estimated-time").innerHTML = `Est BarHop duration: ${totalTimeHours} hours ${totalTimeMins} mins`
-      })
-
-    }
+        this.element.parentElement.parentElement.parentElement.children[0].querySelector(
+          ".estimated-time"
+        ).innerHTML = `Est BarHop duration: ${totalTimeHours} hours ${totalTimeMins} mins`;
+      });
+  }
 }
